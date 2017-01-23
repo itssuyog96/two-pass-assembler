d4fecbca2b4322e46b993dc34c41be78eb95ed56
Suyog Gadhave <itssuyog96@gmail.com>
2017-01-24 04:13:19 +0530

    look

--- sample.asm
+++ sample.asm
@@ -3,10 +3,10 @@
 
 //Comment after a line
 XYZ 	START 	0
--		USING 	*,15
--		L		1,FIVE
--		A		1,FOUR
--		ST		TEMP
+-	USING 	*,15
+-	L		1,FIVE
+-	A		1,FOUR
+-	ST		TEMP
 FIVE	DC		F'4'
 FOUR	DC		F'5'
 TEMP	DS		1F
